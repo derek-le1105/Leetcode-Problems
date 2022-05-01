bce3b5a0db1645849cf09c3fc877b3b0b976cd02
Derek Le <64372005+derek-le1105@users.noreply.github.com>
2022-04-30 23:48:10 -0700

    Time: 360 ms (99.21%), Space: 23.9 MB (64.38%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hashset = set(nums)
+        if len(hashset) < len(nums):
+            return True
+        else:
+            return False
