79e9f6485322642edd0a8ee9358fd4895c3ad823
Derek Le <64372005+derek-le1105@users.noreply.github.com>
2022-09-19 12:37:07 -0700

    Time: 54 ms (39.19%), Space: 13.9 MB (11.57%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -1,23 +1,14 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def middleNode(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        count = 1
-        tempHead = head.next
-        while tempHead != None:
-            count += 1
-            tempHead = tempHead.next
-        for i in range(count/2):
-            head = head.next
-        return head
-        
-        #since we don't know what the length of the linked list is, we have two pointers where both are pointing to first node
-        #we traverse the linked list with the first pointer to see how long the linked list is
-        #after finding out the length, use the second pointer to get the middle of the linked list using the length
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast, slow = head, head
+        while head.next != None:
+            slow = slow.next;
+            head = head.next;
+            if head.next != None:
+                head = head.next;
+        return slow
